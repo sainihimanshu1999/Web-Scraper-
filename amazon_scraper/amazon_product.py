1a30893406cbff96d6963711608bd030bcdfc1ca
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-10-26 11:11:10 +0530

    Project 1 of amazon scrapper done

---
+++ amazon_scraper/amazon_product.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
+def amazon(url):
+    res = requests.get(url, headers=headers)
+    # print(res)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    name = soup.select('#title')[0].getText().strip()
+
+    details = [soup.select('#feature-bullets .a-list-item')[i].get_text().strip() for i in range(0,len(soup.select('#feature-bullets .a-list-item')))]
+
+    try:
+        price = soup.select('#priceblock_ourprice')[0].get_text().strip()
+    except:
+        price = soup.select('#priceblock_dealprice')[0].get_text().strip()
+
+    print(f"\n{name}\n {price}")
+    for detail in details:
+        print(detail,end="\n")
+
+amazon(input('Enter the url\n'))
+
+
